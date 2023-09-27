e652db0f3ebcdee734b8f1d8698f7e09caf08d30
aks024680 <130197640+aks024680@users.noreply.github.com>
2023-09-27 16:08:52 +0800

    update

---
+++ Assets/Script/DataSourceController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Grid
+{
+    /// <summary>
+    /// 資料Grid
+    /// </summary>
+    public class DataSourceController : MonoBehaviour
+    {
+        [SerializeField, Header("敵人數值資料"), Range(1, 1000)]
+        private int dataNum;
+
+        private void Update()
+        {
+            OnCalculation();
+        }
+        public int OnCalculation()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                print("1111111");
+                dataNum = Random.Range(0, dataNum);
+                return dataNum;
+            }
+        }
+    }
+}
+
