772c91544c5eeca6e02d143f403e3468e26454af
aks024680 <130197640+aks024680@users.noreply.github.com>
2023-10-13 14:03:28 +0800

    update

--- Assets/Scripts/DataSourceController.cs
+++ Assets/Scripts/DataSourceController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Reflection;
+using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.UI;
 
 
@@ -13,7 +15,7 @@ namespace Grid
         public int changeDataNum;
         [SerializeField, Header("固定數值資料"), Range(1, 1000)]
         public int stoneDataNum;
-        
+
 
         ChangeEnemy enemy;
 
@@ -24,52 +26,56 @@ namespace Grid
         public Button SecondEnemyBtn;
         public Button ThirdEnemyBtn;
 
+        public int bigger, smaller;
 
         private int akan = 1;
-        public Text enemyText,playerText;
+        public Text enemyText, playerText;
 
         private void Start()
         {
             EnemyValue(stoneDataNum);
-           enemy = GetComponentInChildren<ChangeEnemy>();
+            enemy = GetComponentInChildren<ChangeEnemy>();
+            print("1");
         }
 
         public void OnMouseDown()
         {
             print(changeDataNum);
 
-            CompareValue();
+            CompareValue(bigger,smaller);
             PlayerValue(changeDataNum);
-            
+
             print("hahahahahahahahahahahahahahahahahahahhahah");
             //myImage.sprite = Resources.Load("Image/Enemy/aphid", typeof(Sprite)) as Sprite;
         }
         public void PlayerValue(int value)
         {
             while (akan > 0)
-            { 
+            {
                 value = changeDataNum;
                 changeDataNum = Mathf.Clamp(value, minVal, maxVal);
                 playerText.text = changeDataNum.ToString();
-                CompareValue();
+                CompareValue(bigger,smaller);
                 akan--;
+                print("12");
             }
         }
         public void EnemyValue(int enemyValue)
         {
             enemyValue = stoneDataNum;
             enemyText.text = stoneDataNum.ToString();
+            print("1235");
         }
-        public bool CompareValue()
+        public bool CompareValue(int bigger,int smaller)
         {
-            if (changeDataNum > stoneDataNum)
+            if (bigger > smaller)
             {
                 changeDataNum += stoneDataNum;
 
                 print("123");
 
                 return true;
-                
+
             }
             else
             {
@@ -77,20 +83,20 @@ namespace Grid
                 return false;
             }
         }
-        /*public void OnClick()
-        {
-            
-            FirstEnemyBtn.gameObject.SetActive(false);
-        }
-        public void OnClick2()
-        {
-            
-            SecondEnemyBtn.gameObject.SetActive(false);
-        }
-        public void OnClick3()
-        {
-            
-            ThirdEnemyBtn.gameObject.SetActive(false);
-        }*/
+        //public void OnClick()
+        //{
+
+        //    FirstEnemyBtn.gameObject.SetActive(false);
+        //}
+        //public void OnClick2()
+        //{
+
+        //    SecondEnemyBtn.gameObject.SetActive(false);
+        //}
+        //public void OnClick3()
+        //{
+
+        //    ThirdEnemyBtn.gameObject.SetActive(false);
+        //}
     }
 }
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,43 @@
+﻿
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Grid
+{
+    public class SaveData : MonoBehaviour
+    {
+        public Text firstText;
+        public Text secondText;
+        public Text thirdText;
+
+        public Button FirstEnemyBtn;
+        public Button SecondEnemyBtn;
+        public Button ThirdEnemyBtn;
+        
+         
+        public int bigger, smaller;
+
+        DataSourceController dataSourceController;
+        private void Awake()
+        {
+            ClickFirstButton();
+            dataSourceController = GetComponent<DataSourceController>();
+        }
+       
+        public void ScoreCompare()
+        {
+            FirstEnemyBtn.onClick.AddListener(ClickFirstButton);
+        }
+        public void ClickFirstButton()
+        {
+            if (dataSourceController.CompareValue(bigger,smaller)) 
+            { 
+                print("1234");
+                FirstEnemyBtn.gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
+
